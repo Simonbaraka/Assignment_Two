5eadb622aef7ce0b8394f318d708cb002de7694d
Simon <Simon@SBARAKA>
2025-12-02 21:35:51 +0300

    changes made

---
+++ assigment_2.js
@@ -0,0 +1,6 @@
+//String concatenation
+let names = "Simon"
+let location =" Mowbray"
+let product = "Five bed room house "
+let finalMessage = `Dealer:${names} Location${location}Product:${product}`
+console.log(finalMessage)
