52f96c09b70b848b0261e2aef037c094d47dcb0b
Simon <Simon@SBARAKA>
2025-12-02 22:17:08 +0300

    Made more changes

--- assigment_2.js
+++ assigment_2.js
@@ -28,3 +28,32 @@ console.log(trimmer)
 //split
 let splitter = products.split(" ")
 console.log(splitter)
+
+//replace
+let place = products.replace("Five", "Eight")
+console.log(place)
+
+
+//Type checking and conversion
+
+//string
+let word1 = typeof("Words")
+console.log(word1)
+//number
+let word2= typeof(123)
+console.log(word2)
+//boolean
+let word3 = typeof(true)
+console.log(word3)
+
+
+//String conversion
+//String to Number
+let num = "123"
+let num1 = parseInt(num)
+console.log(num1)
+
+//number to string
+let num2= 123
+let num3 = parseInt(num2)
+console.log(num3)
