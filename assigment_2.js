3fdbc8c44ab4330e05cd99ffc4dac147280ceaf6
Simon <Simon@SBARAKA>
2025-12-02 21:56:47 +0300

    changes made

--- assigment_2.js
+++ assigment_2.js
@@ -2,5 +2,29 @@
 let names = "Simon"
 let location =" Mowbray"
 let product = "Five bed room house "
-let finalMessage = `Dealer:${names} Location${location}Product:${product}`
+let finalMessage = `Dealer: ${names},  Location:${location}, Product: ${product}.`
 console.log(finalMessage)
+
+//String concatenation using the + Operation
+let name1 = "Baraka"
+let location1 ="Rondebosch"
+let product1 = "Complex Apartment"
+let finalMessage1 = "Dealer: " +name1 +"\nLocation: "+location1 +"\nProduct: "+product1
+console.log(finalMessage1)
+
+// String method Challenge
+let products = "Five Bed room house "
+let upper = products.toUpperCase()
+console.log(upper)
+
+//toLowerCase
+let lower = products.toLowerCase()
+console.log(lower)
+
+//trim
+let trimmer = products.trim()
+console.log(trimmer)
+
+//split
+let splitter = products.split(" ")
+console.log(splitter)
